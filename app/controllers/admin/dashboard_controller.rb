745ee5e3a091835d4ede922c709cc40fe4d3aebb
Neriman Dikmen <sanerneriman@gmail.com>
2023-11-09 19:38:20 +0300

    implement admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name:  Rails.configuration.admin[:name], password: Rails.configuration.admin[:password]
 
   def show
+    @product_count = Product.all()
+    @category_count = Category.all()
   end
 end
