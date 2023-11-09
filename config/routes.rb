ad9aab9a89d19908211702957dc9a96d1e860fb3
Neriman Dikmen <sanerneriman@gmail.com>
2023-11-09 21:03:24 +0300

    Add sessions controller, login and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
   get 'about', to: "about#index"
   root to: 'products#index'
 
